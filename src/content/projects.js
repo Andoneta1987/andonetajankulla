8cc6f60860a3bff25cf679af81245c4729721d6c
Andoneta Jankulla <150067948+Andoneta1987@users.noreply.github.com>
2024-05-05 22:02:36 +0200

    Update projects.js

--- src/content/projects.js
+++ src/content/projects.js
@@ -3,15 +3,15 @@ export const projects=[
         title:"HTML/CSS",
         subtitle:"User Login",
         description:"Html with implementation of css",
-        image:"./images/Css.img.png",
+        image: require('./images/Css.img.png').default,
         link:"/",
     },
     {
         title:"JAVA SCRIPT",
         subtitle:"Calculator",
         description:"Calculating the bill",
-        image:"/images/reactjs.img.png",
-        link:"https://github.com/Andoneta1987/Web-Dev.git",    
+        image:require('./images/reactjs.img.png').default,
+        link:"https://github.com/Andoneta1987/andonetajankulla",    
     },
     {
         title:"REACT JS",
